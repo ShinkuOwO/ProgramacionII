c484381088e49ca1ef68d4f5d26b579833f34acf
Shinku <pgomez519@gmail.com>
2023-08-22 14:09:07 -0400

    mecanica disparo ADD

--- Assets/Scripts/JhonController.cs
+++ Assets/Scripts/JhonController.cs
@@ -11,7 +11,7 @@ public class JhonController : MonoBehaviour
     private bool Suelo;
     Rigidbody2D rb2d;
     Animator animacion;
-    SpriteRenderer sr;
+    public SpriteRenderer sr;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -22,27 +22,12 @@ public class JhonController : MonoBehaviour
     {
         Horizontal = Input.GetAxisRaw("Horizontal"); // -1,0,1 
         animacion.SetBool("corriendo", Horizontal != 0.0f);
-        if (Horizontal > 0)
-        {
-            sr.flipX = false;
-        }
-        else if (Horizontal < 0)
-        {
-            sr.flipX = true;
-        }
+        if (Horizontal > 0){sr.flipX = false;}
+        else if (Horizontal < 0){sr.flipX = true;}
         Debug.DrawRay(transform.position, Vector2.down * raycast, Color.green);       
-        if (Physics2D.Raycast(transform.position, Vector2.down, raycast))
-        {
-            Suelo = true;
-        }
-        else
-        {
-            Suelo = false;
-        }
-        if (Input.GetKeyDown(KeyCode.X) && Suelo)
-        {
-            Salto();
-        } 
+        if (Physics2D.Raycast(transform.position, Vector2.down, raycast)){Suelo = true;}
+        else{Suelo = false;}
+        if (Input.GetKeyDown(KeyCode.X) && Suelo){Salto();} 
     }  
     private void Salto()
     {
